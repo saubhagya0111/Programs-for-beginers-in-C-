0b95c21e3f8db4d0b51d8c67d351d4b061a31926
Saubhagya Chopra <75625298+saubhagya011@users.noreply.github.com>
2021-01-11 00:31:11 +0530

    Create Palindrome.cpp

---
+++ Palindrome.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+/* C++ program to check if a number is a palindrome */
+int main()
+{
+    int rev=0,n,rem;
+    cout<<"\n Enter the number which is to be checked";
+    cin>>n;
+    /* Reversing a number using while loop */
+    while (n!=0)
+    {
+        rem=n%10;
+        rev=rev*10+rem;
+        n/=10;
+    }
+    /* Checking if the input satisfies the definition of the palindrome using if condition */
+    if(n==rev)
+    {
+        cout<<"\n Number is a palindrome";
+   
+    }
+    else 
+    cout<<"\n Number is not a palindrome";
+    return 0;
+    
+
+}
