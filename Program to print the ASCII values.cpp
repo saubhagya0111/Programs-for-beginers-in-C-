57d3b70c7cf96f2c279681cd5a64f99dfb8bac1c
Saubhagya Chopra <75625298+saubhagya011@users.noreply.github.com>
2021-01-11 17:31:06 +0530

    Create Program to print the ASCII values.cpp

---
+++ Program to print the ASCII values.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+/* C++ program to generate ASCII value of charecter entered*/
+int main()
+{
+    char ch;
+    cout<<"\n Enter any charecter whose ASCII value is to be printed"<<endl;
+    cin>>ch;
+    cout<<"\n Corresponding ASCII Value is"<<" ="<<int(ch);
+    return 0;
+}
