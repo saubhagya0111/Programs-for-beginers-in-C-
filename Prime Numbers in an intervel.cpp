1657aff5192ccc27377c0dda402fd361cc94b110
Saubhagya Chopra <75625298+saubhagya011@users.noreply.github.com>
2021-01-15 21:35:57 +0530

    Create Prime Numbers in an intervel.cpp

---
+++ Prime Numbers in an intervel.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n1,n2,flag;
+    //Seeking input from the user
+    cout<<"\n Enter the first number";
+    cin>>n1;
+    cout<<"\n Enter the second number";
+    cin>>n2;
+    cout<<"\n Prime Numbers are "<<endl;
+    //Comparing the largest and the smallest nos
+    int s=n1<n2?n1:n2;
+    int l=n1>n2?n1:n2;
+    //A loop to find the numbers between the intervel
+    for (int i =s;i <=l; i++)
+    {
+        flag=1;
+        //Initialising the flag variable as 1 by default in the loop
+        //Loop to determine the number of factors of each individual number by checking the divisibility
+        for(int j=2;j<=(i/2);j++)
+        {
+            if(i%j==0)
+            {
+                flag=0;
+                break;
+            }
+        }
+        //Displaying the prime numbers using if condition whaen value of flag=0
+        if (flag==1)
+        {
+            cout<<i<<endl;
+
+        }
+        
+    }
+    cout<<"\n Program executed";
+    return 0;
+}
