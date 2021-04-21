2c0ff42c997421b1ba9970659fa610018ff04f46
Saubhagya Chopra <75625298+saubhagya011@users.noreply.github.com>
2021-04-21 12:39:19 +0530

    Create Concatenation(Merging) of String.cpp

---
+++ Concatenation(Merging) of String.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+//Concatenation (Merging) of String
+int main()
+{
+	string s1 = "Saubhagya", s2 = "Chopra";
+	cout << "Merged string-->" << s1 + s2 << endl;
+	return 0;
+}
