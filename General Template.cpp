1f5dabbacc82976b44300569afcd4e8672e06a17
Saubhagya Chopra <75625298+saubhagya0111@users.noreply.github.com>
2021-06-18 13:16:09 +0530

    Create General Template.cpp

---
+++ General Template.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define incr_loop(i, n) for (i = 0; i < n; i++)
+#define decr_loop(i, n) for (i = n; i > 0; i--)
+#define long long int ll
+#define in(x) cin >> x;
+#define out(x) cout << x;
+#define line cout << endl;
+#define string_in(x) getline(cin, x)
+#define f first
+#define s second
+typedef vector<int> vi;
+typedef vector<char> vc;
+mt19937_64 rang(chrono::high_resolution_clock::now().time_since_epoch().count());
+const int mod = 1'000'000'007;
+const int N = 3e5, M = N;
+int main()
+{
+
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    srand(chrono::high_resolution_clock::now().time_since_epoch().count());
+
+    return 0;
+}
